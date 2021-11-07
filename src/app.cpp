887697e2737384bd1ddbef8b293c1a5ac124cecf
CamGHenderson <CamGHenderson@gmail.com>
2021-11-07 00:20:43 -0600

    Create app.cpp

---
+++ src/app.cpp
@@ -0,0 +1,34 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+sf::RenderWindow window;
+
+void handleEvents()
+{
+    sf::Event event;
+    while(window.pollEvent(event))
+    {
+        switch(event.type)
+        {
+            case sf::Event::Closed:
+            {
+                window.close();
+            }
+        }
+    }
+}
+
+int main()
+{
+    window.create(sf::VideoMode(800, 600), "Tank Game");
+    
+    while(window.isOpen())
+    {
+        handleEvents();
+        
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+    
+    return 0;
+}
